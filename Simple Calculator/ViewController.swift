d0341dbea26acfdb0193d0b56d2b44bbc0a6d5e5
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-01-26 21:21:33 +0300

    Initial Commit

---
+++ Simple Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Calculator
+//
+//  Created by Atıl Samancıoğlu on 26/01/2017.
+//  Copyright © 2017 Atıl Samancıoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
